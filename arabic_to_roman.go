f0fe62401911887fbbc52b9f8d1e3fa613b9e728
Shyam Naru <shyam.s.naru@vumc.org>
2018-02-16 08:50:36 -0600

    Convert arabic numerals to roman

---
+++ arabic_to_roman.go
@@ -0,0 +1,58 @@
+package main
+ 
+import (
+    "fmt"
+)
+
+func arabicToRoman(arg int)(string) {
+
+    figure := []int{1000,100,10,1}
+    romanDigitA := []string{
+        1 : "I",
+        10 : "X",
+        100 : "C",
+        1000 : "M",
+    }
+    romanDigitB := []string{
+        1 : "V",
+        10 : "L",
+        100 : "D",
+        1000 : "MMMMM",
+    }
+
+    if arg < 0 || arg > 4000 {
+        return "ROMAN_OUT_OF_RANGE"
+    }
+
+    romanSlice := []string{}
+    x := ""
+
+    for _, f := range figure {
+        digit, i, v := int(arg / f), romanDigitA[f], romanDigitB[f]
+        switch digit {
+            case 1: romanSlice = append(romanSlice, string(i))
+            case 2: romanSlice = append(romanSlice, string(i) + string(i))
+            case 3: romanSlice = append(romanSlice, string(i) + string(i) + string(i))
+            case 4: romanSlice = append(romanSlice, string(i) + string(v))
+            case 5: romanSlice = append(romanSlice, string(v))
+            case 6: romanSlice = append(romanSlice, string(v) + string(i))
+            case 7: romanSlice = append(romanSlice, string(v) + string(i)+ string(i))
+            case 8: romanSlice = append(romanSlice, string(v) + string(i)+ string(i)+ string(i))
+            case 9: romanSlice = append(romanSlice, string(i) + string(x))
+        }
+        arg -= digit * f
+        x = i
+    }
+    ret := ""
+    for _, e := range romanSlice {
+        ret += e
+    }
+    return ret
+}
+ 
+func main() {
+    fmt.Print("Enter Arabic Numerals: ")
+    var arabicNumeral int
+    fmt.Scanln(&arabicNumeral)
+    fmt.Printf("%s: %s\n", "Roman Numeral", arabicToRoman(arabicNumeral))
+}
