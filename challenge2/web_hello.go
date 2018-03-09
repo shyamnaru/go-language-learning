51e6eb117f99df103d7c6db5728acaf4227bc72c
Shyam Naru <shyam.s.naru@vumc.org>
2018-03-08 22:53:35 -0600

    Create basic web hello world

---
+++ challenge2/web_hello.go
@@ -0,0 +1,13 @@
+package main
+
+import("fmt"
+		"net/http")
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello World!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8000", nil)
+}
